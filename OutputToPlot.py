df07e03103781dff4fed3a9a258742c8e00e2324
Jannik <34013062+Ashiven@users.noreply.github.com>
2023-03-02 04:04:16 +0100

    Update OutputToPlot.py

--- OutputToPlot.py
+++ OutputToPlot.py
@@ -1,5 +1,7 @@
 import csv
+import datetime
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 
 def parse_row(row):
     date_str, price_str = row
@@ -18,19 +20,30 @@ with open(filename, 'r', newline='', encoding = 'utf-8') as csvfile:
     for row in reader:
         row_num += 1
         date, price = parse_row(row)
-        dates.append(date)
         if row_num % 2 == 0:
             euros.append(price)
         else:
             dollars.append(price)
+            dates.append(date)
+
+datesp = []
+
+for date_str in dates:
+    date = datetime.datetime.strptime(date_str[:-9], '%Y-%m-%d')
+    datesp.append(date)
 
 # Plot data
 fig, ax = plt.subplots()
-ax.plot(dates, dollars, label='Dollars')
-ax.plot(dates, euros, label='Euros')
+ax.plot(datesp, dollars, label='Dollars')
+ax.plot(datesp, euros, label='Euros')
 ax.set_xlabel('Date')
 ax.set_ylabel('Price')
 ax.set_title('Price over Time')
-fig.autofmt_xdate() # Rotate and align x-axis labels
 ax.legend()
+
+# Format x-axis to only show day, month, and year
+date_form = DateFormatter("%d-%m-%Y")
+ax.xaxis.set_major_formatter(date_form)
+fig.autofmt_xdate() # Rotate and align x-axis labels
+
 plt.show()
