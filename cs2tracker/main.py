8f6d1f65f3f5addf5af0e97b41635dd9b288f3a3
ashiven <nevisha@pm.me>
2023-11-15 03:47:36 +0100

    Update console print message with new version and logo

--- cs2tracker/main.py
+++ cs2tracker/main.py
@@ -9,49 +9,20 @@ def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     console = Console()
+    console.print(
+        "[bold yellow]"
+        + """
+        __   _____ _____  ______  ____    ____     __  __  _    ___  ____
+   /  ] / ___/|     T|      T|    \  /    T   /  ]|  l/ ]  /  _]|    \
+  /  / (   \_ l__/  ||      ||  D  )Y  o  |  /  / |  ' /  /  [_ |  D  )
+ /  /   \__  T|   __jl_j  l_j|    / |     | /  /  |    \ Y    _]|    /
+/   \_  /  \ ||  /  |  |  |  |    \ |  _  |/   \_ |     Y|   [_ |    \
+\     | \    ||     |  |  |  |  .  Y|  |  |\     ||  .  ||     T|  .  Y
+ \____j  \___jl_____j  l__j  l__j\_jl__j__j \____jl__j\_jl_____jl__j\_j
 
-    console.print(
-        "[bold yellow]"
-        + "                                                                              "
-    )
-    console.print(
-        "[bold yellow]"
-        + "    __   _____  ____   ___   ______  ____    ____     __  __  _    ___  ____  "
-    )
-    console.print(
-        "[bold yellow]"
-        + "   /  ] / ___/ /    T /   \ |      T|    \  /    T   /  ]|  l/ ]  /  _]|    \ "
-    )
-    console.print(
-        "[bold yellow]"
-        + "  /  / (   \_ Y   __jY     Y|      ||  D  )Y  o  |  /  / |  ' /  /  [_ |  D  )"
-    )
-    console.print(
-        "[bold yellow]"
-        + " /  /   \__  T|  T  ||  O  |l_j  l_j|    / |     | /  /  |    \ Y    _]|    / "
-    )
-    console.print(
-        "[bold yellow]"
-        + "/   \_  /  \ ||  l_ ||     |  |  |  |    \ |  _  |/   \_ |     Y|   [_ |    \ "
-    )
-    console.print(
-        "[bold yellow]"
-        + "\     | \    ||     |l     !  |  |  |  .  Y|  |  |\     ||  .  ||     T|  .  Y"
-    )
-    console.print(
-        "[bold yellow]"
-        + " \____j  \___jl___,_j \___/   l__j  l__j\_jl__j__j \____jl__j\_jl_____jl__j\_j"
-    )
-    console.print(
-        "[bold yellow]"
-        + "                                                                              "
-    )
-    console.print(
-        "[bold yellow]" + "Version: v2.0.1 - 11/14/2023 - Jannik Novak @ashiven_"
-    )
-    console.print(
-        "[bold yellow]"
-        + "                                                                              "
+
+"""
+        + "Version: v2.0.4 - 11/14/2023 - Jannik Novak @ashiven_"
     )
 
     application = Application()
